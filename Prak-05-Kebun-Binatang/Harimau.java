4ead2405f9bc2f6849b915b912a4ce9024d864d1
Mifthahul Jannah <mita_mj26@yahoo.com>
2015-09-12 17:12:44 +0700

    Praktikum 5 Kebun Binatang

---
+++ Prak-05-Kebun-Binatang/Harimau.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class Harimau here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Harimau implements Mamalia
+{
+    // instance variables - replace the example
+    public String suara()
+    {
+        String suara="Aum";
+        return suara;
+    }
+    public int jumlahKaki(){
+        
+       int jumlahKaki = 4;
+       return jumlahKaki;
+    
+    }}
